a49f90e467537db37ef2b31b699aaf5c7e34584e
DindaJingganM <dindajinggan20@gmail.com>
2021-12-27 20:22:20 +0700

    memulai project

---
+++ php/src/dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("10.0.0.45","admin","Admin123!","tokopekita");
+
+if(!$conn){
+	echo "gagal konek database menn";
+} else {
+	
+};
+
+?>
